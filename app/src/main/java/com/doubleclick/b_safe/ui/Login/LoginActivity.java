d2a297cfed15ab36da53bd99e19f99f906c45a24
eslam <eslamfgazy600@gmail.com>
2022-06-18 02:13:48 +0200

    Finish

--- app/src/main/java/com/doubleclick/b_safe/ui/Login/LoginActivity.java
+++ app/src/main/java/com/doubleclick/b_safe/ui/Login/LoginActivity.java
@@ -8,39 +8,49 @@ import android.os.Bundle;
 
 import com.doubleclick.b_safe.MainActivity;
 import com.doubleclick.b_safe.R;
+import com.doubleclick.b_safe.notification.Api.APIService;
+import com.doubleclick.b_safe.notification.Client;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
 
     private FirebaseAuth auth;
     public FirebaseAuth.AuthStateListener authStateListener;
+    public FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         auth = FirebaseAuth.getInstance();
-        authStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-            }
-        };
+        currentUser = auth.getCurrentUser();
+        if (currentUser != null) {
+            authStateListener = new FirebaseAuth.AuthStateListener() {
+                @Override
+                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+            };
+        }
     }
 
 
     @Override
     protected void onStart() {
         super.onStart();
-        auth.addAuthStateListener(authStateListener);
+        if (currentUser != null) {
+            auth.addAuthStateListener(authStateListener);
+        }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        auth.removeAuthStateListener(authStateListener);
+        if (currentUser != null) {
+            auth.removeAuthStateListener(authStateListener);
+        }
     }
 
 }
